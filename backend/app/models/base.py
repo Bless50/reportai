1e1a3eb900087b9dcfd31623c1d00f58aaa20f33
Bless50 <blessn691@gmail.com>
2024-11-24 20:04:44 +0100

    Added .gitignore file

--- backend/app/models/base.py
+++
@@ -1,8 +0,0 @@
-# import the necessary function from SQLALCHEMY
-# declarative_base() is used to create a base class for our database models
-from sqlalchemy.ext.declarative import declarative_base
-
-# create a base class for our database models
-# this class will be used as a base class for all our database models
-#it provides them  with the necessary functionality to work with the sqlachemy
-Base = declarative_base()
